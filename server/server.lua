ba32a27099af38dc63d7316db1f6a574596f923a
NatasWillDoIt <105885655+NatasWillDoIt@users.noreply.github.com>
2022-12-25 12:59:23 -0800

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -2,6 +2,6 @@ RegisterServerEvent('repair:costRepair', function()
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
         Player.Functions.RemoveMoney('bank', Config.Money, "repair-bill")
-        exports['qb-management']:AddMoney('hayes', Config.Money)
+        exports['qb-management']:AddMoney('mechanic', Config.Money)
         TriggerClientEvent('QBCore:Notify', src, 'Repair Done!!', 'success')
 end)
